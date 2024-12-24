2ac163f199a25e602bda305e89da5ab884572611
Idris Khan <idris.khan303@gmail.comm>
2024-12-24 11:25:25 +0000

    join spawned threads so main thread has to wait to completion

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use rust_atomics::section_1::{
 
 fn main() {
     // section 1
-    thread_main();
+    // thread_main();
     // static_thread_main()
-    // reference_counting_main();
+    reference_counting_main();
     // data_races_main();
     // interior_mutability_cell_main();
 }
--- src/section_1/reference_counting.rs
+++ src/section_1/reference_counting.rs
@@ -6,11 +6,18 @@ pub fn reference_counting_main() {
 
     assert_eq!(a.as_ptr(), b.as_ptr());
 
+    /*Rc are not thread safe and not atomic, instead use Arc which is thread safe and atomic */
     // thread::spawn(move || dbg!(b));
 
     let a = Arc::new([1, 2, 3]);
     let b = a.clone();
 
-    thread::spawn(move || dbg!(a));
-    thread::spawn(move || dbg!(b));
+    // both threads get their own Arc (so there are 4 Arcs that all point to the same array)
+    let t1 = thread::spawn(move || dbg!(a));
+    let t2 = thread::spawn(move || dbg!(b));
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    // last thread to see Arc go to 0 will drop and deallocate memory
 }
